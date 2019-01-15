6360d1af8b0f75d278e53a1c4655788a7d7c2042
Mamadou Amar <mamadou.amar-garcia@etu.u-pec.fr>
2019-01-15 18:31:04 +0100

    Initial commit

--- hello-app/src/main/java/me/client/App.java
+++ hello-app/src/main/java/me/client/App.java
@@ -1,25 +1,67 @@
 package me.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.CheckBox;
+import com.google.gwt.user.client.ui.RootPanel;
 import com.intendia.rxgwt2.elemento.RxElemento;
-import elemental2.dom.HTMLElement;
+import elemental2.dom.*;
+import io.reactivex.Observable;
+import io.reactivex.Observer;
 import org.jboss.gwt.elemento.core.Elements;
 
 import static elemental2.dom.DomGlobal.document;
 import static org.jboss.gwt.elemento.core.EventType.mousemove;
+import static org.jboss.gwt.elemento.core.EventType.touchmove;
 
 
 public class App implements EntryPoint {
 
     @Override
     public void onModuleLoad() {
-        HTMLElement el= Elements.span().style("font-weight:bold;").get();
+        HTMLElement el = Elements.div().style("width:655px;text-align:center;background-color: coral;").get();
+
         Elements.body().add("mouse position:").add(el);
-        RxElemento.fromEvent(document,mousemove).subscribe(ev->el.textContent=ev.clientX +";"+ ev.clientY);
-        //Elements.body().add("Hello World");
-        /*
-        Text textNode = Document.get().createTextNode("Hello World!");
-        RootPanel.getBodyElement().appendChild(textNode);
-        */
+
+
+        CheckBox showCoordinates = new CheckBox("Activate coordinates");
+        showCoordinates.setValue(false);
+
+
+        showCoordinates.addClickHandler(new ClickHandler() {
+
+            Observable<RxElemento> mouseMoveCoordinates = new Observable<RxElemento>() {
+                @Override
+                protected void subscribeActual(Observer<? super RxElemento> observer) {
+                    RxElemento.fromEvent(document, mousemove).subscribe(mouseEvent -> el.textContent = mouseEvent.clientX + "," + mouseEvent.clientY);
+
+                }
+            };
+
+            Observable<RxElemento> touchMoveCoordinates = new Observable<RxElemento>() {
+                @Override
+                protected void subscribeActual(Observer<? super RxElemento> observer) {
+                    RxElemento.fromEvent(document, touchmove).subscribe(touchEvent -> el.textContent = "Pressed");
+
+                }
+            };
+
+            @Override
+            public void onClick(ClickEvent event) {
+
+                boolean checked = ((CheckBox) event.getSource()).getValue();
+                if (checked) {
+
+                    mouseMoveCoordinates = mouseMoveCoordinates.mergeWith(touchMoveCoordinates);
+                    mouseMoveCoordinates.subscribe();
+
+                } else {
+                    RxElemento.fromEvent(document, mousemove).subscribe(ev -> el.textContent = "");
+                }
+            }
+        });
+        RootPanel.get().add(showCoordinates);
     }
+
 }
